7ac84d6fb9f1d64dfea23895be31be7f26e968d2
Alejandro Gallo <aamsgallo@gmail.com>
2024-09-16 01:10:34 +0200

    Add some tests

---
+++ el-who-test.el
@@ -0,0 +1,127 @@
+;;; el-who-test.el --- Test for el-who               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Alejandro Gallo
+
+;; Author: Alejandro Gallo <gallo@unug>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'el-who)
+
+(ert-deftest el-who-test-simple ()
+  "Simple transformations"
+
+  (should (string= (el-who `(:br))
+                   "<br/>"))
+
+  (should (string= (el-who `(:div))
+                   "<div></div>"))
+
+  (should (string= (el-who `(:input :type "text"))
+                   "<input type=\"text\"/>"))
+
+  (should (string= (el-who `(:input :type "text"
+                                    :placeholder "Somethig"
+                                    :required t
+                                    :value "Hello World"))
+                   "<input type=\"text\" placeholder=\"Somethig\" required value=\"Hello World\"/>"))
+
+  (should (string= (el-who `(:p (:p (:span) (:span (:p) (:span)))))
+                   "<p>
+<p>
+<span></span><span>
+<p></p><span></span>
+</span>
+</p>
+</p>")))
+
+(ert-deftest el-who-test-cl:who ()
+  "Tests for cl:who compattibility"
+
+  (should (string=
+           (el-who-cl-who
+            (:div
+             (:p)
+             (loop for i below 4
+                   do (who:htm (:span :id i
+                                      (who:fmt "Hello %s" i))))))
+           "<div>
+<p></p><span id=\"0\">
+Hello 0
+</span><span id=\"1\">
+Hello 1
+</span><span id=\"2\">
+Hello 2
+</span><span id=\"3\">
+Hello 3
+</span>
+</div>"))
+
+  (should (string=
+           (el-who-cl-who
+            (:div :class "card w-100"
+                  (let ((card "hello")
+                        (deck "world"))
+                    (who:htm
+                     (:div (who:str card)
+                           (:div :class "card-footer"
+                                 (:form :method "POST"
+                                        :action "/grade"
+                                        (:input :name "deck-id"
+                                                :value deck
+                                                :class "d-none invisible")
+                                        (:input :name "card-id"
+                                                :value deck
+                                                :class "d-none invisible")
+                                        (:span :class "badge fs-7 bg-secondary"
+                                               (who:fmt "This %s" card))
+                                        (:div :class "btn-group float-end"
+                                              (loop for i below 5
+                                                    do (who:htm
+                                                        (:button :class "btn btn-primary"
+                                                                 :value i
+                                                                 :name "grade"
+                                                                 (who:str i))))))))))))
+           "<div class=\"card w-100\">
+<div>
+hello<div class=\"card-footer\">
+<form method=\"POST\" action=\"/grade\">
+<input name=\"deck-id\" value=\"world\" class=\"d-none invisible\"/><input name=\"card-id\" value=\"world\" class=\"d-none invisible\"/><span class=\"badge fs-7 bg-secondary\">
+This hello
+</span><div class=\"btn-group float-end\">
+<button class=\"btn btn-primary\" value=\"0\" name=\"grade\">
+0
+</button><button class=\"btn btn-primary\" value=\"1\" name=\"grade\">
+1
+</button><button class=\"btn btn-primary\" value=\"2\" name=\"grade\">
+2
+</button><button class=\"btn btn-primary\" value=\"3\" name=\"grade\">
+3
+</button><button class=\"btn btn-primary\" value=\"4\" name=\"grade\">
+4
+</button>
+</div>
+</form>
+</div>
+</div>
+</div>")))
+
+(provide 'el-who-test)
+;;; el-who-test.el ends here
--- el-who.el
+++ el-who.el
@@ -20,66 +20,122 @@
 
 (require 'cl-lib)
 
-(defun el-who (tree)
-  "Create an html string representation of the s-expression TREE."
+
+(defun el-who--engine (tree)
+  "Write a HTML representation of the s-expression TREE in the current buffer.
+
+It writes the output HTML code into the current buffer, so you can
+decide using the `with-current-buffer' macro to redirect the output
+somewhere else."
   (cl-flet ((kwd-name (kwd) (substring (symbol-name kwd) 1)))
     (pcase tree
+
+      ;;
+      ;;
+      ;; Handle general tags
+      ;;
+      ;;
       ((and `(,tag . ,rest)
             (guard (keywordp tag)))
        (let ((i 0)
              (void-tag-p (member tag '(:input
                                        :br
                                        :img))))
-         (string-join
-          (append
 
-           ;; Write beginning tag
-           (list (format "<%s" (kwd-name tag)))
+         ;;
+         ;; Write beginning tag
+         ;;
 
-           ;; Write attributes if any
-           (let ((attrs
-                  (cl-loop for (k v) on rest by #'cddr
-                        collect
-                        (pcase k
-                          ((guard (keywordp k))
-                           (prog1 (when v
-                                    (cond
-                                      ((eq v t) (format "%s"
-                                                        (kwd-name k)))
-                                      (t
-                                       (format "%s=%S" (kwd-name k) v))))
-                             (cl-incf i)
-                             (cl-incf i)))
-                          ;; if there is no match return immediately
-                          (_ (cl-return))))))
-             (when attrs
-               (list (concat " "
-                             (string-join attrs " ")))))
+         (insert (format "<%s" (kwd-name tag)))
+
+         ;;
+         ;; Write attributes if any
+         ;;
+         (cl-loop for (k v) on rest by #'cddr
+               collect
+               (pcase k
+                 ((guard (keywordp k))
+                  (prog1 (when v
+                           (insert " ")
+                           (cond
+                             ((eq v t) (insert (format "%s"
+                                                       (kwd-name k))))
+                             (t
+                              (insert (format "%s=%S"
+                                              (kwd-name k)
+                                              (format "%s" (eval v)))))))
+                    (cl-incf i)
+                    (cl-incf i)))
+                 ;; if there is no match return immediately
+                 (_ (cl-return))))
 
 
-           ;; Write a closing > tag if it's a normal tag
-           ;; or /> if it's a ,,void-tag''
-           (list (if void-tag-p "/>\n" ">\n"))
+         ;;
+         ;; Write a closing > tag if it's a normal tag
+         ;; or /> if it's a ,,void-tag''
+         ;;
 
-           ;; render the body of the tag
-           (cl-loop for j from i to (length rest)
-                 collect (el-who (nth j rest)))
+         (insert (if void-tag-p "/>" ">"))
 
-           ;; close the tag properly if we have to
-           (unless void-tag-p
-             (list (format "\n</%s>\n" (kwd-name tag))))))))
+         ;;
+         ;; render the body of the tag
+         ;;
+         (unless (= (length rest) i)
+           (newline))
+         (cl-loop for j from i to (length rest)
+               do (el-who--engine (nth j rest)))
+         (unless (= (length rest) i)
+           (newline))
 
+         ;;
+         ;; close the tag properly if we have to
+         ;;
+
+         (unless void-tag-p
+           (insert (format "</%s>" (kwd-name tag))))))
+
+      ;;
+      ;;
       ;; if it's an atom, just get the formatted version of it
+      ;;
+      ;;
       ((and (pred atom)
             (guard (not (null tree))))
-       (format "%s" tree))
+       (insert (format "%s" tree)))
 
+      ;;
+      ;;
       ;; if it's something resembling a lisp call, just evaluate the
       ;; tree and pass the return value through el-who again
+      ;;
+      ;;
       ((and `(,form-name . ,_)
             (guard (symbolp form-name)))
-       (el-who (eval tree))))))
+       (el-who--engine (eval tree))))))
 
+(defun el-who (tree)
+  "Create an html string representation of the s-expression TREE."
+  (with-temp-buffer
+    (el-who--engine tree)
+    (buffer-string)))
+
+(defmacro el-who-htm (tree)
+  "A macro recreating the cl:who functionality for TREE."
+  `(el-who--engine ',tree))
+
+(defmacro el-who-cl-who (tree)
+  "Transform a S-expression TREE which include cl-who functions.
+
+The cl-who functions are `who:str', `who:htm' and `who:fmt'.
+Also loop keywords are replaced by the cl-lib symbol `cl-loop'.
+It returns a string containing the HTML output.
+Bear in mind that `who:fmt' is a quite bad replacement by the elisp function
+`format' since the formatting strings are not the same."
+  `(el-who (cl-sublis '((who:str . identity)
+                        (who:htm . el-who-htm)
+                        (who:fmt . format)
+                        (loop . cl-loop))
+                      ',tree)))
 
 (provide 'el-who)
 
